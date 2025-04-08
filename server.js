6f48e15f31699d7c68b6b36f77ced072950ab578
jpthimt <joaopedrothim@gmail.com>
2025-04-08 10:06:50 -0300

    Commit before render web service.

--- server.js
+++ server.js
@@ -1,33 +1,53 @@
-// import {createServer} from 'node:http'
-
-// const server = createServer((request, response) => {
-//     response.write('Hello World')
-
-//     return response.end()
-// })
-
-// server.listen(3333)
-
 import { fastify } from 'fastify'
+// import { DatabaseMemory } from './database-memory.js'
+// import { describe } from 'node:test'
+import { DatabasePostgres } from './database-postgres.js'
 
 const server = fastify()
 
-server.post('/videos', () => {
-    return 'Hello World'
+// const database = new DatabaseMemory()
+const database = new DatabasePostgres()
+
+server.post('/videos', async (request, reply) => {
+    const { title, description, duration } = request.body
+
+    await database.create({
+        title,
+        description,
+        duration,
+    })
+    return reply.status(201).send()
 })
 
-server.get('/videos', () => {
-    return 'Hello!'
+server.get('/videos', async (request) => {
+    const search = request.query.search
+
+    const videos = await database.list(search)
+
+    return videos
 })
 
-server.put('/videos/:id', () => {
-    return 'Hello Node'
+server.put('/videos/:id', async (request, reply) => {
+    const videoId = request.params.id
+    const { title, description, duration } = request.body
+
+    await database.update(videoId, {
+        title,
+        description,
+        duration,
+    })
+
+    return reply.status(204).send()
 })
 
-server.delete('/videos/:id', () => {
-    return 'Hello Node'
+server.delete('/videos/:id', async (request, reply) => {
+    const videoId = request.params.id
+
+    await database.delete(videoId)
+
+    return reply.status(204).send()
 })
 
 server.listen({
-    port: 3333,
+    port: process.env.PORT ?? 3333,
 })
